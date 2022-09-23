56d5c16a5af26644f8a0c6217fd9686774e6f2dd
Lyam[BRS] <cous5830@gmail.com>
2022-09-23 16:20:41 -0400

    - Release after changing assembly info.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BRS_Dallas_Programmer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Open source Serial Terminal and Dallas (MAXIM) chip autonomous programmer. Made by Lyam under BRS for a school project.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BRS_Dallas_Programmer")]
+[assembly: AssemblyCompany("BRS")]
+[assembly: AssemblyProduct("Dallas Programmer & Serial Terminal")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("82760eae-526c-4b0e-b52e-cfd1807cd4e1")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.2.5")]
+[assembly: AssemblyFileVersion("1.0.2.5")]
